5b23556f88bc18a7eb7710252a87e75a1f8f3480
Cosmin <cr.rusucosmin@gmail.com>
2017-10-07 11:51:44 -0700

    Added encrypt and decrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,55 @@ class ApplicationController < ActionController::Base
     render template: 'lab1'
   end
   def encrypt
-    # TODO(rusucosmin): add here the encryption logic
-    render html: params["1"].to_s + params[:text].to_s
+    # TODO(rusucosmin): add server-side validation
+    ord = {}
+    Array(0..26).each do |x|
+      ord[params[x.to_s].to_s] = x
+    end
+    alpha = params[:alpha].to_i
+    beta = params[:beta].to_i
+    text = params[:text]
+    logger.info "Encrypting " + text
+    logger.info "alpha " + alpha.to_s
+    logger.info "beta " + beta.to_s
+    encrypted = text.split("")
+        .map{|x| ord[x]}
+        .map{|id| id == 26 ? 26 : (alpha * id + beta) % 26}
+        .map{|ide| params[ide.to_s]}
+        .join
+    logger.info "Encrypted to " + encrypted
+    render template: 'encrypt', :locals => { :encrypted => encrypted, :text => text }
+  end
+
+  def get_inverse(alpha, m)
+    Array(0..m).each do |x|
+      if (x * alpha) % m == 1
+        return x
+      end
+    end
+    return nil
   end
 
   def decrypt
-    # TODO(rusucosmin): add here the decryption logic
+    # TODO(rusucosmin): add server-side validation
+    ord = {}
+    Array(0..26).each do |x|
+      ord[params[x.to_s].to_s] = x
+    end
+    alpha = params[:alpha].to_i
+    beta = params[:beta].to_i
+    ciphertext = params[:ciphertext]
+    logger.info "Dencrypting " + ciphertext
+    logger.info "alpha " + alpha.to_s
+    logger.info "beta " + beta.to_s
+    alpha_inv = get_inverse(alpha, 26)
+    logger.info "alpha_in " + alpha_inv.to_s
+    decrypted = ciphertext.split("")
+        .map{|ch| ord[ch]}
+        .map{|id| id == 26 ? 26 : (alpha_inv * (id - beta + 26)) % 26}
+        .map{|idx| params[idx.to_s]}
+        .join
+    logger.info "Decrypted: " + decrypted.to_s
+    render template: "decrypt", :locals => { :decrypted => decrypted, :ciphertext => ciphertext }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'application#show'
   post '/encrypt' => 'application#encrypt'
+  post '/decrypt' => 'application#decrypt'
 end
