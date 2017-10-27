8122bcda48f5074100c8f7abe8ee16708c52eb46
Cosmin <cr.rusucosmin@gmail.com>
2017-10-27 14:15:16 +0300

    Fixed

--- README.md
+++ README.md
@@ -7,8 +7,22 @@ Ruby version: 2.5.2
 Rails version: 5.1.4
 
 # Documentation
-## Laboratory 1
-- Assigned cipher: Affine cipher
+## Laboratory 1 - Affine cipher
+* The affine cipher is a type of monoalphabetic substitution cipher, wherein
+each letter in an alphabet is mapped to its numeric equivalent, encrypted using
+a simple mathematical function, and converted back to a letter. The formula used
+means that each letter encrypts to one other letter, and back again, meaning the
+cipher is essentially a standard substitution cipher with a rule governing which
+letter goes to which. As such, it has the weaknesses of all substitution
+ciphers. Each letter is enciphered with the function (ax + b) mod 26, where b is
+the magnitude of the shift.
+* A particular example of the affine cipher is the caesar cipher (with a = 1 and
+b = 1).
+* The encryption function is E(x) = (a * x + b) mod m
+* The decryption function is D(x) = a<sup>-1</sup>  * (x - b) mod m
+* Where (a, b) is the key of the affine cipher
+* m is the length of the alphabet
+* gcd(a, m) == 1
 
 # Run locally
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,18 +18,26 @@
 //= require_tree .
 
 $(document).ready(function(){
+  // variable to keep what action we need to perform (encrypt / decrypt)
   isEncrypt = true;
+  // initialize with default parameters
   populate(validate())
+  // don't let the user introduce other special characters
   $("#alphabet").keypress(function(e) {
     return e.which === 32 || (e.which >= 97 && e.which <= 122)
   })
+  // whenever a user action occurs, we validate the inputs
   $("#form input").on("input", function() {
     populate(validate())
   })
+  // toggle the encrypt / decrypt
   $("#locker").click(function() {
+    // enable animations
     $(".lock-box").toggleClass("lock-box-active")
     $(".lock-circle").toggleClass("lock-circle-active")
+    // flip the ecrypt variable
     isEncrypt = !isEncrypt
+    // populate with default text
     if (isEncrypt) {
       $("#title").text("Affine cipher - Encrypt")
       $("#text").val("text to encrypt")
@@ -41,22 +49,35 @@ $(document).ready(function(){
       $("#text-label").text("Text to decrypt")
       $("#result-label").text("Decrypted")
     }
+    // validate the inputs
     populate(validate())
   })
+  // method returns the modular inverse of alpha mod m
+  // alpha - the number to get the modular inverse
+  // m - we work modulo m
+  // method returns the modular inverse or -1 if there is no such number
   function getModularInverse(alpha, m) {
     for (inv = 0; inv < m; ++ inv) {
-      if ((alpha * inv) % m == 1)
-        return alpha
+      if ((alpha * inv) % m === 1) {
+        console.log("inv mod of " + alpha + " is " + inv)
+        //return alpha
+        return inv
+      }
     }
     return -1;
   }
+  // method decrypts the text with the affine cipher key (alpha, beta)
+  // operations are performed on the alphabet 'alphabet'
   function decrypt(alphabet, alpha, beta, text) {
     idx = {}
     for (x = 0; x < alphabet.length; ++ x) {
       idx[alphabet[x]] = x
     }
+    // ehb
     dec = ""
+    console.log("alpha = " + alpha)
     inv = getModularInverse(alpha, alphabet.length)
+    console.log("inv modular = " + inv)
     if (x == -1) {
       alert ("No modular inverse for alpha and length of alphabet")
       return ;
@@ -66,8 +87,10 @@ $(document).ready(function(){
       newind = inv * (ind - beta + alphabet.length) % alphabet.length;
       dec += alphabet[newind];
     }
-    return dec
+    return dec.toUpperCase()
   }
+  // method encrypts the text with the affine cipher key (alpha, beta)
+  // operations are performed on the alphabet 'alphabet'
   function encrypt(alphabet, alpha, beta, text) {
     idx = {}
     for (x = 0; x < alphabet.length; ++ x) {
@@ -79,8 +102,9 @@ $(document).ready(function(){
       newind = (alpha * ind + beta) % alphabet.length
       enc += alphabet[newind]
     }
-    return enc
+    return enc.toUpperCase()
   }
+  // auxiliary method to perform the encryption / decryption
   function populate(valid) {
     if (valid == true) {
       if (isEncrypt) {
@@ -98,12 +122,14 @@ $(document).ready(function(){
       $("#result").val("")
     }
   }
+  // method returns the greatest commond divisor of a and b
   function gcd(a, b) {
     if (b == 0) {
       return a
     }
     return gcd(b, a % b)
   }
+  // check if the text contains only unique letters
   function unique(txt) {
     obj = {}
     for (i = 0; i < txt.length; ++ i) {
@@ -113,6 +139,7 @@ $(document).ready(function(){
     }
     return true
   }
+  // method checks for the conditions
   function validate() {
     valid = true
     // check the alphabet
